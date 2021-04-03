de9251532f918351f67a6f54ea6ddc04a0a452f3
Sparkenstein <24642451+Sparkenstein@users.noreply.github.com>
2021-04-03 13:22:15 +0530

    add: multiplexing panes and sizes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require 'wezterm';
 
-
 local mykeys = {}
 for i = 1, 9 do
     table.insert(mykeys, {
@@ -19,15 +18,53 @@ end
 table.insert(mykeys, { key="[", mods="CTRL", action=wezterm.action{ActivateTabRelative=-1}})
 table.insert(mykeys, { key="]", mods="CTRL", action=wezterm.action{ActivateTabRelative=1}})
 
+table.insert(mykeys, { key="V", mods="ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}} )
+table.insert(mykeys, { key="H", mods="ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}} )
+
+table.insert(mykeys, { key="b", mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}} )
+table.insert(mykeys, { key="n", mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}} )
+
+-- Pane activation when in split panes mode
+table.insert(mykeys, { key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"}} )
+table.insert(mykeys, { key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"}} )
+table.insert(mykeys, { key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"}} )
+table.insert(mykeys, { key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"}} )
+
+-- Pane sizes
+table.insert(mykeys, { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}})
+table.insert(mykeys, { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}})
+table.insert(mykeys, { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}})
+table.insert(mykeys, { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}})
+
+--        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+--        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+--        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
+
 return {
   -- Fonts
   font = wezterm.font("FantasqueSansMono Nerd Font"),
   font_size = 15.0,
+  line_height = 1.2,
 
+  -- Colors
+  bold_brightens_ansi_colors = true,
+  inactive_pane_hsb = {
+        hue = 1.0,
+        saturation = 0.7,
+        brightness = 0.6,
+  },
   -- UI
-  color_scheme = "Andromeda",
+  color_scheme = "Dracula+",
 
   -- Keybindings
-  keys = mykeys
+  leader = { key = "m", mods="CTRL" },
+  keys = mykeys,
+
+  -- Commands configuration
+  launch_menu = {
+    {
+        args = {"btm"},
+    }
+  },
 }
 
